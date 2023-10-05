2548cc356a59dbdb1ed94333be9aab54b4542bd5
LyHoangPhuc <146864195+LyHoangPhuc@users.noreply.github.com>
2023-10-05 16:51:34 +0700

    initial commit

--- QuanLyQuanAn/FAccountProfile.cs
+++ QuanLyQuanAn/FAccountProfile.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QuanLyQuanAn.DAO;
+using QuanLyQuanAn.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,16 +14,52 @@ namespace QuanLyQuanAn
 {
     public partial class FAccountProfile : Form
     {
-        public FAccountProfile()
+        private Account loginAccount;
+        public Account LoginAccount
         {
-            InitializeComponent();
+            get { return loginAccount; }
+            set { loginAccount = value; changeAccount(loginAccount); }
         }
 
+        public FAccountProfile(Account acc)
+        {
+            InitializeComponent();
+            loginAccount = acc;
+        }
+
+        void changeAccount(Account acc)
+        {
+            txbUserName.Text = loginAccount.UserName;
+            txbDisPlayName.Text = loginAccount.DisplayName;
+        }
+        void UpdateAccount()
+        {
+            string displayName = txbDisPlayName.Text;
+            string password = txbPassWord.Text;
+            string newpass = txbNewPass.Text;
+            string reenterPass = txbReEnterPass.Text;
+            string userName = txbUserName.Text;
+            if (!newpass.Equals(reenterPass))
+            {
+                MessageBox.Show("Vui lonhf nhập lại mật khẩu đúng với mật khẩu mới!");
+            }
+            else
+            {
+                if (AccountDAO.Instance.UpdateAccount(userName, displayName, password, newpass))
+                {
+                    MessageBox.Show("Cập nhật thành công");
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng điền đúng mật khẩu");
+                }
+            }
+        }
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        
+
     }
 }
