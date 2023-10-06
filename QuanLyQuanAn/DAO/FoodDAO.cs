740a90f0e933fbfe87535f03b3aeaad715ab52b4
Phat-1909 <Phatdothanh1909@gmail.com>
2023-10-06 23:34:26 +0700

    git push

--- QuanLyQuanAn/DAO/FoodDAO.cs
+++ QuanLyQuanAn/DAO/FoodDAO.cs
@@ -31,5 +31,21 @@ namespace QuanLyQuanAn.DAO
             }
             return List;
         }
+        public List<Food> GetListFood()
+        {
+            List<Food> list = new List<Food>();
+
+            string query = "select * from Food";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                Food food = new Food(item);
+                list.Add(food);
+            }
+
+            return list;
+        }
     }
 }
